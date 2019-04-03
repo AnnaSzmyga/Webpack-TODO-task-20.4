adefc2280d173e6cce6c5fb2a379a78eee60e4f7
Anna Szmyga <anna.szmyga@o2.pl>
2019-04-03 09:45:03 +0200

    Correct 'TodoForm' component

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -14,6 +14,7 @@ class TodoForm extends React.Component {
     onSubmitHandle(event) {
         event.preventDefault();
         this.props.addTodo(this.state.newTodoText);
+        this.setState({newTodoText: ''});
     }
     render() {
         return(
